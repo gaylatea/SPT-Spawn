57a2687fcab1fe69f4b8a89c4c1f480c7797c687
Ari <a@silversupre.me>
2023-04-01 14:20:59 -0700

    Get rid of all those coroutines

--- ActivatePatch.cs
+++ ActivatePatch.cs
@@ -38,125 +38,97 @@ namespace Framesaver
                     ___action_0(bot);
                 }
 
-                Activate(bot);
+                var actualActivate = typeof(BotOwner)?.GetMethod("method_10", BindingFlags.Instance | BindingFlags.NonPublic);
+                var spawnedOnOddFrame = Plugin.isOnOddFrame();
+
+                // This cursed line is required in order to properly set the odd frame flag on an extant unused boolean
+                // left on the BotOwner class.
+                typeof(BotOwner)?.GetField("bool_1", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(bot, spawnedOnOddFrame);
+
+                Logger.LogInfo(String.Format("Framesaver is taking control of bot {0}, spawned on odd frame: {1}", bot.ProfileId, spawnedOnOddFrame));
+
+                bot.Transform.position = bot.BotsGroup.BotZone.SpawnPoints.RandomElement<ISpawnPoint>().Position + Vector3.up * 0.5f;
+                actualActivate.Invoke(bot, null);
                 return false;
             }
 
             return true;
         }
-
-        public static bool isOnOddFrame()
-        {
-            return ((Time.frameCount % 2) == 1);
-        }
-
-        // Activate more efficiently activates a bot by avoiding the more
-        // complicated pathfinding calculations that are done in vanilla,
-        // as well as spreading them out throughout the frames.
-        public static async void Activate(BotOwner bot)
-        {
-            var actualActivate = typeof(BotOwner)?.GetMethod("method_10", BindingFlags.Instance | BindingFlags.NonPublic);
-            var spawnedOnOddFrame = isOnOddFrame();
-
-            Logger.LogInfo(String.Format("Framesaver is taking control of bot {0}, spawned on odd frame: {1}", bot.ProfileId, spawnedOnOddFrame));
-
-            while (bot.WeaponManager == null || !bot.WeaponManager.IsReady)
-            {
-                await Task.Yield();
-            }
-
-            bot.Transform.position = bot.BotsGroup.BotZone.SpawnPoints.RandomElement<ISpawnPoint>().Position + Vector3.up * 0.5f;
-            actualActivate.Invoke(bot, null);
-
-            await RunLoop(bot, spawnedOnOddFrame);
-        }
-
-        // RunLoop is a more efficient way of running the Bot code so that it
-        // doesn't take up all the time in a frame.
-        public static async Task RunLoop(BotOwner bot, bool spawnedOnOddFrame)
-        {
-            var nextGoalCalcTime = Time.time;
-
-            for (; ; )
-            {
-                // Do not update bots that aren't alive.
-                if (bot.BotState != EBotState.Active) { await Task.Yield(); continue; }
-
-                // If a bot is dead, then just stop processing entirely.
-                if (!bot.GetPlayer.HealthController.IsAlive)
-                {
-                    await Task.Yield(); break;
-                }
-
-                bot.StandBy.Update();
-                bot.LookSensor.UpdateLook();
-
-                // Don't update bots that are paused, for whatever reason.
-                if (bot.StandBy.StandByType == BotStandByType.paused) { await Task.Yield(); continue; }
-
-                // Has enough time passed for the bot to try and figure out what
-                // its new goal should be?
-                if (nextGoalCalcTime < Time.time)
-                {
-                    await Task.Run(() => { bot.CalcGoal(); });
-                    nextGoalCalcTime = Time.time + 3.0f;
-                }
-
-                // Bots spawned on odd frames will only update on odd frames,
-                // and similarly with bots spawned on even frames.
-                if (spawnedOnOddFrame ^ isOnOddFrame())
-                {
-                    bot.HeadData.ManualUpdate();
-                    bot.Tilt.ManualUpdate();
-                    bot.NightVision.ManualUpdate();
-                    bot.CellData.Update();
-                    bot.FriendChecker.ManualUpdate();
-                    bot.TrianglePosition.ManualUpdate();
-                    bot.Medecine.ManualUpdate();
-                    bot.Boss.ManualUpdate();
-                    bot.BotTalk.ManualUpdate();
-                    bot.BotRequestController.Update();
-                    bot.Tactic.UpdateChangeTactics();
-                    bot.Memory.ManualUpdate(Time.deltaTime);
-                    bot.Settings.UpdateManual();
-                    bot.BotRequestController.TryToFind();
-
-                    bot.ShootData.ManualUpdate();
-                    bot.DogFight.ManualUpdate();
-                    bot.RecoilData.LosingRecoil();
-                    bot.AimingData.PermanentUpdate();
-
-                    if (bot.WeaponManager != null)
-                    {
-                        bot.WeaponManager.ManualUpdate();
-                    }
-
-                    bot.Mover.ManualUpdate();
-                    if (bot.GetPlayer.UpdateQueue == EUpdateQueue.Update)
-                    {
-                        bot.Mover.ManualFixedUpdate();
-                        bot.Steering.ManualFixedUpdate();
-                    }
-                }
-
-                await Task.Yield();
-            }
-        }
     }
 
-    // UpdatePatch removes the original handling for updating bots, which causes
-    // quite a lot of performance issues.
     public class UpdatePatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
         {
-            return typeof(BotsClass)?.GetMethod("UpdateByUnity", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            return typeof(BotOwner)?.GetMethod("UpdateManual", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
         }
 
         [PatchPrefix]
-        public static bool PatchPrefix()
+        public static bool PatchPrefix(ref BotOwner __instance, float ___float_0, float ___float_3, bool ___bool_1)
         {
-            return !Plugin.Enabled.Value;
+            // Do not update bots that aren't alive.
+            if (__instance.BotState != EBotState.Active) { return false; }
+
+            // If a bot is dead, then just stop processing entirely.
+            if (!__instance.GetPlayer.HealthController.IsAlive)
+            {
+                return false;
+            }
+
+            __instance.StandBy.Update();
+            __instance.LookSensor.UpdateLook();
+
+            // Don't update bots that are paused, for whatever reason.
+            if (__instance.StandBy.StandByType == BotStandByType.paused) { return false; }
+
+            // Has enough time passed for the bot to try and figure out what
+            // its new goal should be?
+            if (___float_0 < Time.time)
+            {
+                __instance.CalcGoal();
+            }
+
+            if (___bool_1 ^ Plugin.isOnOddFrame())
+            {
+                __instance.ShootData.ManualUpdate();
+                __instance.DogFight.ManualUpdate();
+                __instance.RecoilData.LosingRecoil();
+                __instance.AimingData.PermanentUpdate();
+
+                if(__instance.WeaponManager != null) {
+                    __instance.WeaponManager.ManualUpdate();
+                }
+
+                __instance.HeadData.ManualUpdate();
+                __instance.Tilt.ManualUpdate();
+                __instance.NightVision.ManualUpdate();
+                __instance.CellData.Update();
+                __instance.FriendChecker.ManualUpdate();
+                __instance.TrianglePosition.ManualUpdate();
+                __instance.Medecine.ManualUpdate();
+                __instance.Boss.ManualUpdate();
+                __instance.BotTalk.ManualUpdate();
+                __instance.BotRequestController.Update();
+                __instance.Tactic.UpdateChangeTactics();
+                __instance.Memory.ManualUpdate(Time.deltaTime);
+                __instance.Settings.UpdateManual();
+                __instance.BotRequestController.TryToFind();
+
+                __instance.Mover.ManualUpdate();
+                if (__instance.GetPlayer.UpdateQueue == EUpdateQueue.Update)
+                {
+                    __instance.Mover.ManualFixedUpdate();
+                    __instance.Steering.ManualFixedUpdate();
+                }
+            }
+
+
+            // This isn't needed for our use cases, I'm not even sure why it
+            // would be included in the production code.
+            //__instance.UnityEditorRunChecker.ManualLateUpdate();
+
+            // Always skip the original bot code.
+            return false;
         }
     }
 
--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,11 @@ namespace Framesaver
     [BepInPlugin("com.gaylatea.framesaver", "SPT-Framesaver", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static bool isOnOddFrame()
+        {
+            return ((Time.frameCount % 2) == 1);
+        }
+
         internal static ConfigEntry<bool> Enabled;
 
         void Awake()
